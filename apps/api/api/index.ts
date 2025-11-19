1ea0758c43ab0d5ebc77a63ddcbda72bea32a7cb
atticusjxn <atticusjxn@gmail.com>
2025-11-19 19:34:58 +1000

    fix(api): load fastify server for vercel

--- apps/api/api/index.ts
+++ apps/api/api/index.ts
@@ -1,9 +1,9 @@
-import type { IncomingMessage, ServerResponse } from 'node:http';
+import type { VercelRequest, VercelResponse } from '@vercel/node';
 import { buildServer } from '../dist/server.js';
 
 const app = buildServer();
+await app.ready();
 
-export default async function handler(req: IncomingMessage, res: ServerResponse) {
-  await app.ready();
+export default async function handler(req: VercelRequest, res: VercelResponse) {
   app.server.emit('request', req, res);
 }
